671331f2fd7a8553220bc4408ee10ed578ad4d48
jrngpar <jrngpar@gmail.com>
2017-03-29 14:52:20 -0700

    Update proj01.py
    Done. Don't know how to format a table

--- proj01.py
+++ proj01.py
@@ -1,1 +1,20 @@
+#get Richter scale input that accepts float
+#return energy in joules
+#return energy in tons of TNT
 
+def get_in():
+	richter = float(input("Richter scale measurement: "))
+	print("Richter measurement: {}".format(richter))
+	return richter
+
+def calculate_energy(richter):
+	energy = 10**((1.5*richter)+4.8)
+	print("Joules: {}".format(energy))
+	return energy
+
+def calculate_tnt(energy):
+	tons_tnt = energy/(4.184e9)
+	print("Tons of TNT: {}".format(tons_tnt))
+	return tons_tnt
+	
+calculate_tnt(calculate_energy(get_in()))
